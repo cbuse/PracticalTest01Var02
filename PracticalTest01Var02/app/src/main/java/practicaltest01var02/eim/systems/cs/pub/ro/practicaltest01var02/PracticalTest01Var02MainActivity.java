cdaf55d498739e4d9f544ddae4644aaed1a9479e
Cristina Buse <cristina_buse@yahoo.com>
2016-03-28 14:16:26 +0300

    proba

---
+++ PracticalTest01Var02/app/src/main/java/practicaltest01var02/eim/systems/cs/pub/ro/practicaltest01var02/PracticalTest01Var02MainActivity.java
@@ -0,0 +1,13 @@
+package practicaltest01var02.eim.systems.cs.pub.ro.practicaltest01var02;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class PracticalTest01Var02MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var02_main);
+    }
+}
